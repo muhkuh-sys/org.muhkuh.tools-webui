361e4beb9c5ce0cc731ab901c425f88fd38008b0
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-11-15 11:45:28 +0100

    Handle SVG files as images.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         loader: 'style-loader!css-loader!sass-loader'
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [{
           loader: 'file-loader',
           options: {
@@ -32,11 +32,13 @@ module.exports = {
         }]
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader',
-            options: {}
+            options: {
+              outputPath: 'img/'
+            }
           }
         ]
       }
