62c53acc114d108482ff549b59bc356aae09f741
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-09-06 14:19:16 +0200

    Write the generated artifaces to "targets".

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname,'src','index.js'),
+  entry: path.join(__dirname, 'src', 'index.js'),
 
   output: {
-    path: path.resolve(__dirname, "lua_server", "www")
+    path: path.resolve(__dirname, "targets", "www")
   },
 
   module: {
