c7ccd6d37f84945a62c8a506de20ecdb3d398fff
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-08-11 16:06:14 +0200

    Set the initial focus to the start button and make it visible with the focus ripple.

--- local/jsx/test_start.jsx
+++ local/jsx/test_start.jsx
@@ -1,6 +1,14 @@
 class Interaction extends React.Component {
   constructor(props) {
     super(props);
+
+    this.initialFocus = null;
+  }
+
+  componentDidMount() {
+    if( this.initialFocus!==null ) {
+      this.initialFocus.focusVisible();
+    }
   }
 
   handleButtonStart = () => {
@@ -19,7 +27,7 @@ class Interaction extends React.Component {
         </div>
 
         <div style={{width: '100%', textAlign: 'center'}}>
-          <Button color="primary" variant="contained" size="large" onClick={this.handleButtonStart}>
+          <Button color="primary" variant="contained" size="large" onClick={this.handleButtonStart} autoFocus action={actions => { this.initialFocus = actions; }}>
             <SvgIcon>
               <path d="M8 5v14l11-7z"/><path d="M0 0h24v24H0z" fill="none"/>
             </SvgIcon>
